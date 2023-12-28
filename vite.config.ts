b59394bead91b182538f6466d64b8819b332c3c7
zhangqian <zhangqian1@hyperchain.cn>
2023-12-28 15:49:27 +0800

    feat: 添加 Icon 组件，配置 alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     open: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/icons")],
+      symbolId: "[name]",
+    }),
+  ],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      {
+        find: "components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+    ],
+  },
 })
