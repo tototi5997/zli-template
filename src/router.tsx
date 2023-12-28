bad2735e069d2c69254f9705c6fb289457327cc8
zhangqian <zhangqian1@hyperchain.cn>
2023-12-28 18:21:55 +0800

    feat: router config 添加注释

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
-import Foo from "./layout/foo"
 import { HashRouter, BrowserRouter } from "react-router-dom"
+import Foo from "./layout/foo"
 
 export type RouterType = "hash" | "browser"
 
+// 路由类型
 const ROUTER_TYPE = "hash"
 
+// 路由配置
 const routerConfig = [
   {
     path: "/",
