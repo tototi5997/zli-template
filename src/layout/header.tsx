1e0689a93548e5405f36ea42a2762d34c9612115
tototi5997 <873440829@qq.com>
2024-05-07 17:22:05 +0800

    feat: Add zh-cn lang control in version

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,18 +1,32 @@
-import { Badge } from "antd"
-import UserPopover from "@/pages/userPopover"
-import IconButton from "@/components/iconButton"
-import Icon from "@/components/icon"
-import User from "@/components/user"
-import c from "classnames"
-import s from "./index.module.less"
+import { Badge } from "antd";
+import UserPopover from "@/pages/userPopover";
+import IconButton from "@/components/iconButton";
+import Icon from "@/components/icon";
+import User from "@/components/User";
+import c from "classnames";
+import s from "./index.module.less";
+import { useTranslation } from "react-i18next";
+import { useCallback } from "react";
+
+enum LangeType {
+  ZN = "zh-CN",
+  EN = "en",
+}
 
 const Header = () => {
+  const { t, i18n } = useTranslation();
+
+  const curLang = i18n.language;
+
+  const handleChangeLang = useCallback(() => i18n.changeLanguage(curLang === LangeType.ZN ? LangeType.EN : LangeType.ZN), [curLang, i18n]);
+
   return (
     <div className={c(s.header, "h-50 w-full flex items-center p-20")}>
       <Icon name="favicon" />
-      <div className="fs16 pl28">Platfrom Name</div>
+      <div className="fs16 pl28">{t("platformName")}</div>
 
       <section className={c(s.header_right, "flex gap-20")}>
+        <IconButton name="lang" onClick={handleChangeLang} />
         <Badge count={10} dot size="small">
           <IconButton name="alarm" title="Infomation" />
         </Badge>
@@ -20,7 +34,7 @@ const Header = () => {
         <User className={c(s.user_icon)} popContent={UserPopover} />
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
